6c83037a1a62e3a392ebf1ba5b3db3071828c98e
Loan-Coal <lohanncolle2022@gmail.com>
2025-01-10 12:15:36 +0100

    fixed some stuff

--- code_asm/parmTesting2.py
+++ code_asm/parmTesting2.py
@@ -8,6 +8,8 @@ def file_convert(file_path):
             str += (assemble_instruction(lines[lineIndex][:-1]) + " ")
     return str
 
+
+#PROBLEM SOME OF THE INSTRUCTIONS HAVE SAME NAME DIFFERET OPCODE: NEED TO USE ARUGMENTS
 def write_string_to_txt_file(string, file_name):
     with open(file_name, 'w') as file:
         file.write(string)
@@ -15,12 +17,45 @@ def write_string_to_txt_file(string, file_name):
 def assemble_instruction(instruction):
     # Define the instruction set and their binary opcodes
     instruction_set = {
-        'movs': '00100',
-        'ands': '0100000000',
-        'eors': '0100000001',
-        'lsls': '0100000010',
-        'lsrs': '0100000011',
-        # Add more instructions as needed
+        "lsls": ["00000", "0100000010"],
+        "lsrs": ["00001", "0100000011"],
+        "asrs": ["00010", "0100000100"],
+        "adds": "0001100",
+        "subs": "0001101",
+        "movs": "00100",
+        "ands": "0100000000",
+        "eors": "0100000001",
+        "adcs": "0100000010",
+        "sbcs": "0100000011",
+        "rors": "0100000111",
+        "tst":  "0100001000",
+        "rsbs": "0100001001",
+        "cmp":  "0100001010",
+        "cmn":  "0100001011",
+        "orrs": "0100001100",
+        "muls": "0100001101",
+        "bics": "0100001110",
+        "mvns": "0100001111",
+        "str": "10010",
+        "ldr": "10011",
+        "add": "101100000",
+        "sub": "101100001",
+        "b": "11011110",
+        "beq": "11010000",
+        "bne": "11010001",
+        "bcs": "11010010",
+        "bcc": "11010011",
+        "bmi": "11010100",
+        "bpl": "11010101",
+        "bvs": "11010110",
+        "bvc": "11010111",
+        "bhi": "11011000",
+        "bls": "11011001",
+        "bge": "11011010",
+        "blt": "11011011",
+        "bgt": "11011100",
+        "ble": "11011101",
+        "bal": "1110"
     }
 
     # Function to convert register and immediate values to binary
@@ -38,15 +73,41 @@ def assemble_instruction(instruction):
 
     # Initialize the binary code
     binary_code = ''
-
     if instr == 'movs':
         rd = int(operands[0][1:])
         imm8 = int(operands[1][1:])
         binary_code = f"{instruction_set[instr]}{to_binary(rd, 3)}{to_binary(imm8, 8)}"
-    elif instr in ['ands', 'eors', 'lsls', 'lsrs']:
+
+
+    elif instr in ["lsls", "lsrs", "asrs"]: 
+        rd = int(operands[0][1:])
+        rm = int(operands[1][1:])
+        if (len(operands) == 3): 
+            imm5 = int(operands[2][1:])
+            binary_code = f"{instruction_set[instr][0]}{to_binary(imm5, 5)}{to_binary(rm, 3)}{to_binary(rd, 3)}"
+        elif (len(operands) == 2): 
+            binary_code = f"{instruction_set[instr][1]}{to_binary(rm, 3)}{to_binary(rd, 3)}"
+
+
+    elif instr in ['ands', 'eors', 'adcs', 'sbcs', 'rors', 'tst', 'rsbs', 'cmp', 'cmn', 'orrs', 'muls', 'bics', 'mvns']:
         rd = int(operands[0][1:])
         rm = int(operands[1][1:])
         binary_code = f"{instruction_set[instr]}{to_binary(rm, 3)}{to_binary(rd, 3)}"
+    elif instr in ['adds', 'subs']:
+        rd = int(operands[0][1:])
+        rn = int(operands[1][1:])
+        rm = int(operands[2][1:])
+        binary_code = f"{instruction_set[instr]}{to_binary(rm, 3)}{to_binary(rn, 3)}{to_binary(rd, 3)}"
+    elif instr in ['str', 'ldr']:
+        rt = int(operands[0][1:])
+        imm8 = int(operands[1][4:-1])
+        binary_code = f"{instruction_set[instr]}{to_binary(rt, 3)}{to_binary(imm8, 8)}"
+    elif instr in ['add_sp', 'sub_sp']:
+        imm7 = int(operands[1][1:])
+        binary_code = f"{instruction_set[instr]}{to_binary(imm7, 7)}"
+    elif instr in ['b', 'beq', 'bne', 'bcs', 'bcc', 'bmi', 'bpl', 'bvs', 'bvc', 'bhi', 'bls', 'bge', 'blt', 'bgt', 'ble', 'bal']:
+        imm8 = int(operands[0][1:])
+        binary_code = f"{instruction_set[instr]}{to_binary(imm8, 8)}"
     else:
         raise ValueError("Instruction not supported")
 
@@ -55,7 +116,14 @@ def assemble_instruction(instruction):
     return hex_code
 
 
+
 def fullConvert(inputPathString, outPathString): 
     write_string_to_txt_file(file_convert(inputPathString), outPathString)
 
-fullConvert("C:\\Users\\Lohann\\Desktop\\parmTest.s", "C:\\Users\\Lohann\\Desktop\\testResultHexa.txt")
+#fullConvert("C:\\Users\\Lohann\\Desktop\\parmTest.s", "C:\\Users\\Lohann\\Desktop\\testResultHexa.txt")
+
+
+
+fullConvert("C:\\Users\\Lohann\\Desktop\\test2.s", "C:\\Users\\Lohann\\Desktop\\testResultHexa2.txt")
+
+
